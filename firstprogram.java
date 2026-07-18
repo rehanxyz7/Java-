6da75e897826df7f51eaf4d1a08c827b5c851ef3
rehanxyz7 <rehanshil.net@gmail.com>
2026-07-18 13:11:37 +0530

    New Commit

---
+++ firstprogram.java
@@ -0,0 +1,26 @@
+class Mobile{
+    String name;
+    int model;
+    int year;
+    void Switch(){
+        System.out.println("Switch off ");
+
+    }
+    Mobile(String n, int m,int y){
+        name =n;
+        model =m;
+        year=y;
+    }
+}
+
+class firstprogram {
+public static void main(String[]abc){
+    Mobile x = new Mobile("Motorola ", 2,2021);
+    Mobile y=  new Mobile("Apple ", 8,2016);
+
+    System.out.println(x.name);
+    System.out.println(y.model);
+    x.name= "Oppo";
+    System.out.println(x.name);
+}    
+}
