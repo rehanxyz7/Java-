c757226798b67b9f71a66fee686c8976bdd1fdcc
rehanxyz7 <rehanshil.net@gmail.com>
2026-07-31 01:42:41 +0530

    New Commit

---
+++ Javafoundation/datatypes.java
@@ -0,0 +1,17 @@
+
+public class datatypes {
+    public static void main(String[] abc){
+        int age = 9;
+        char c = 'R';
+        short s = 4;
+        double v = 44.323;
+        float wv= 343.34f;
+
+        System.out.println(age);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(v);
+        System.out.println(wv);
+    }
+    
+}
