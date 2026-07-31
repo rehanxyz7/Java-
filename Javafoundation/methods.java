2120e5df6610edcfdf8cd0d506858ffd8e8f97ac
rehanxyz7 <rehanshil.net@gmail.com>
2026-08-01 01:13:09 +0530

    New Commit

---
+++ Javafoundation/methods.java
@@ -0,0 +1,16 @@
+public class methods {
+    public static int sum(int n, int y){
+        return n+y;
+    }
+    public static int sub(int n, int y){
+        return n-y;
+    }
+    
+public static void main(String[]abs){
+    
+System.out.println(sum(5,4));
+System.out.println(sub(6,3));
+
+}
+    
+}
