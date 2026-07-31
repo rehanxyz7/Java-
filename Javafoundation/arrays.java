e7d7db9c88bbbfa2ac40b410ad92a3eb9dc1bf98
rehanxyz7 <rehanshil.net@gmail.com>
2026-08-01 01:14:15 +0530

    New commit

---
+++ Javafoundation/arrays.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class arrays {
+
+    public static void main(String[] args) {
+      //  int [] arr = {44,43,5,6,8,33};
+      //  System.out.println(arr[0]);
+       // System.out.println(arr[1]);
+       // System.out.println(arr[2]);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array :");
+        int n = sc.nextInt();
+
+        int [] arr = new int [n];
+        for(int i =0;i<n;i++){
+            System.out.println("Enter the element "+i);
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("Printing the entered array: ");
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
